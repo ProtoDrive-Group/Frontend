92d26658a5cd132ef737a0435d21d7eef075fe10
pallabez <pallabsonowal999@gmail.com>
2023-10-20 22:43:07 +0530

    add token header and change tsx to jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,8 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 import ThemeProvider from '@/provider/ChakraProvider'
 import ReduxProvider from '@/provider/ReduxProvider'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <ReduxProvider>
--- src/store/pushNoteApi.js
+++ src/store/pushNoteApi.js
@@ -4,7 +4,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const pushNoteApi = createApi({
   reducerPath: 'pushNoteApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://pushnote-api-production.up.railway.app/api/v1' }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: 'https://pushnote-api-production.up.railway.app/api/v1',
+    prepareHeaders: (headers) => {
+      headers.set('authorization', 
+        `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk4MjQ3MTM0LCJpYXQiOjE2OTc4MTUxMzQsImp0aSI6IjRjZjM3Y2JkYmYzMzRhNWU5OTQ4M2EwODljZDlhYzYzIiwidXNlcl9pZCI6IjljMjRmZjJhLTM4MDMtNGYzMy1hM2YyLTNhZjE5MTRhYWE2YyJ9.d4w4zUcp5Bx64-qUYGhaE_pakx7Wt2l5hFvtI6kU7tA`
+      )
+
+      return headers
+    }
+  }),
   endpoints: (builder) => ({
     taskList: builder.query({
       query: () => `/tasks`,
